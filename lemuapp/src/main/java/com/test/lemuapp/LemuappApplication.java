8ad51ba8be8ff93315413b58f5e0cf211cafacfe
AlokKumar <AlokKumar@192.168.0.11>
2023-12-24 23:22:02 -0600

    test

--- lemuapp/src/main/java/com/test/lemuapp/LemuappApplication.java
+++ lemuapp/src/main/java/com/test/lemuapp/LemuappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LemuappApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(LemuappApplication.class, args);
 	}
 
